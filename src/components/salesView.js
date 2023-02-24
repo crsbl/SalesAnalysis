0326424d162b57730df19ec13d0bfee1e5267b1f
cortega <cortega@acciontip.cl>
2023-02-24 16:38:19 -0300

    salesanalysis

--- src/components/salesView.js
+++ src/components/salesView.js
@@ -1,7 +1,6 @@
 import "../styles/salesView/index.css";
 import functionShowProducts from "./functionShowProducts";
 import { useSelector } from "react-redux";
-import { useEffect, useState } from "react";
 
 const SalesView = () => {
   const selector = useSelector((state) => state.showProducts);
@@ -16,21 +15,43 @@ const SalesView = () => {
         <h3>Marca</h3>
         <h3>Precio</h3>
       </div>
+
       <div>
-        {functionShowProducts.filterShowProducts(selector).map((listFilterShowProducts) => {
-          return (
-            <div>
-              <h3>{listFilterShowProducts.dateSales}</h3>
-              <h3>{listFilterShowProducts.itemName}</h3>
-              <h3>{listFilterShowProducts.sellername}</h3>
-              <h3>{listFilterShowProducts.TypesProductsName}</h3>
-              <h3>{listFilterShowProducts.productBrandName}</h3>
-              <h3>${listFilterShowProducts.itemPrice}</h3>
-            </div>
-          );
-        })}
+        {functionShowProducts
+          .filterShowProducts(selector)
+          .map((listFilterShowProducts) => {
+            return (
+              <div>
+                <h3>{listFilterShowProducts.dateSales}</h3>
+                <h3>{listFilterShowProducts.itemName}</h3>
+                <h3>{listFilterShowProducts.sellername}</h3>
+                <h3>{listFilterShowProducts.TypesProductsName}</h3>
+                <h3>{listFilterShowProducts.productBrandName}</h3>
+                <h3>${listFilterShowProducts.itemPrice}</h3>
+              </div>
+            );
+          })}
       </div>
 
+      <div>
+      
+        <div>
+          <h3>Cantidad:</h3>
+          <h3>{functionShowProducts.filterShowProducts(selector).length}</h3>
+        </div>
+
+        <div>
+        <h3>Total:</h3>
+        <h3>
+          {functionShowProducts
+            .filterShowProducts(selector)
+            .reduce((acc, Value) => (acc += Value.itemPrice), 0)}
+        </h3>
+      </div>
+
+      </div>
+
+    
     </div>
   );
 };
